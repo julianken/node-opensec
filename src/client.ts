559377c5c993583037dcb1412a55200d9afd5b4e
julianken <julian.kennon.d@gmail.com>
2024-09-14 20:09:42 -0700

    dry up request code and bubble up errors

--- src/client.ts
+++ src/client.ts
@@ -15,30 +15,21 @@ class Client {
     this.apiKey = apiKey;
   }
 
-  async getLegislators(state: StateAbbreviation): Promise<Legislator[]> {
-    const uriProps: GetLegislatorParams = {
-      id: state,
-      method: 'getLegislators',
-      output: 'json',
-      apikey: this.apiKey,
-    };
-
+  private async fetchData<T>(uriProps: Record<string, string>, validationFn: (data: T) => boolean, errorMsg: string): Promise<T> {
     const uri: string = buildOpenSecretsUri(uriProps);
 
     try {
       const response = await fetch(uri);
-
       if (!response.ok) {
         throw new APIError('Network response was not ok', response.status, await response.json());
       }
 
-      const data: LegislatorsResponse = await response.json();
-
-      if (!data.response || !data.response.legislator) {
-        throw new ValidationError('Invalid response format', ['Expected legislator data to be present']);
+      const data: T = await response.json();
+      if (!validationFn(data)) {
+        throw new ValidationError('Invalid response format', [errorMsg]);
       }
 
-      return data.response.legislator.map((legislator: LegislatorWrapper) => legislator['@attributes']);
+      return data;
     } catch (error) {
       if (error instanceof OpenSecretsError) {
         throw error;
@@ -48,6 +39,26 @@ class Client {
         throw new APIError('Network response was not ok', 500, null);
       }
     }
+  }
+
+  async getLegislators(state: StateAbbreviation): Promise<Legislator[]> {
+    const uriProps: GetLegislatorParams = {
+      id: state,
+      method: 'getLegislators',
+      output: 'json',
+      apikey: this.apiKey,
+    };
+
+    const validationFn = (data: LegislatorsResponse) =>
+      !!data.response && !!data.response.legislator;
+
+    const data = await this.fetchData<LegislatorsResponse>(
+      uriProps,
+      validationFn,
+      'Expected legislator data to be present'
+    );
+
+    return data.response.legislator.map((legislator: LegislatorWrapper) => legislator['@attributes']);
   }
 
   async getCandidateSummary(cid: string, cycle?: string): Promise<CandidateSummary> {
@@ -59,31 +70,16 @@ class Client {
       apikey: this.apiKey,
     };
 
-    const uri: string = buildOpenSecretsUri(uriProps);
+    const validationFn = (data: CandidateSummaryResponse) =>
+      !!data.response && !!data.response.summary;
 
-    try {
-      const response = await fetch(uri);
+    const data = await this.fetchData<CandidateSummaryResponse>(
+      uriProps,
+      validationFn,
+      'Expected summary data to be present'
+    );
 
-      if (!response.ok) {
-        throw new APIError('Network response was not ok', response.status, await response.json());
-      }
-
-      const data: CandidateSummaryResponse = await response.json();
-
-      if (!data.response || !data.response.summary) {
-        throw new ValidationError('Invalid response format', ['Expected summary data to be present']);
-      }
-
-      return data.response.summary['@attributes'];
-    } catch (error) {
-      if (error instanceof OpenSecretsError) {
-        throw error;
-      } else if (error instanceof Error && error.message === 'Network Error') {
-        throw new NetworkError('Network Error', uri);
-      } else {
-        throw new APIError('Network response was not ok', 500, null);
-      }
-    }
+    return data.response.summary['@attributes'];
   }
 
   async getCandidateContributions(cid: string, cycle?: string): Promise<Contributor[]> {
@@ -95,31 +91,16 @@ class Client {
       apikey: this.apiKey,
     };
 
-    const uri: string = buildOpenSecretsUri(uriProps);
+    const validationFn = (data: CandidateContributionsResponse) =>
+      !!data.response && !!data.response.contributors && !!data.response.contributors.contributor;
 
-    try {
-      const response = await fetch(uri);
+    const data = await this.fetchData<CandidateContributionsResponse>(
+      uriProps,
+      validationFn,
+      'Expected contributors data to be present'
+    );
 
-      if (!response.ok) {
-        throw new APIError('Network response was not ok', response.status, await response.json());
-      }
-
-      const data: CandidateContributionsResponse = await response.json();
-
-      if (!data.response || !data.response.contributors || !data.response.contributors.contributor) {
-        throw new ValidationError('Invalid response format', ['Expected contributors data to be present']);
-      }
-
-      return data.response.contributors.contributor.map((c: ContributorWrapper) => c['@attributes']);
-    } catch (error) {
-      if (error instanceof OpenSecretsError) {
-        throw error;
-      } else if (error instanceof Error && error.message === 'Network Error') {
-        throw new NetworkError('Network Error', uri);
-      } else {
-        throw new APIError('Network response was not ok', 500, null);
-      }
-    }
+    return data.response.contributors.contributor.map((c: ContributorWrapper) => c['@attributes']);
   }
 }
 
